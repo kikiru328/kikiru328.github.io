69f77a2de6546c7a006307baec2cde173ced093b
kikiru328 <kikiru328@gmail.com>
2022-10-10 21:41:29 +0900

    reset

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "한번에 됐으면 좋겠다 좀",
-        "excerpt":"WHY IT DOESNT WORK MY GOD  왜왜왜오애ㅗ애ㅗ애ㅗ애ㅗ애왜  ","categories": ["Text"],
-        "tags": ["Text","Blog"],
-        "url": "/text/Test/",
-        "teaser": null
-      }]
