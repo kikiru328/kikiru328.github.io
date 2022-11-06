64b254be61297c1076c23f773bf6e4804ce28171
김광훈 <danielkim@gimgwanghun-ui-noteubug.local>
2022-11-06 12:01:36 +0900

    first post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[Thought] 무엇을 해야할까",
+        "excerpt":"비전공자의 개발자는 어떻게 해야할까   ","categories": ["Thought"],
+        "tags": ["Thought","Essay"],
+        "url": "http://localhost:4000/thought/Thought/",
+        "teaser": null
+      }]
