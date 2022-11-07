1250a5c3da49e4ccdfdedda74bdd0d934897a134
kikiru328 <kikiru328@gmail.com>
2022-11-07 18:21:39 +0900

    thpoughts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "[Thought] 무엇을 해야할까",
-        "excerpt":"비전공자의 개발자는 어떻게 해야할까   ","categories": ["Thought"],
+        "excerpt":"Introduction 끄적끄적 일기마냥 생각나는 아무거나.. 지금부터 해야하는 것들 ~~생각보다 많겠지?~~ 인공지능 인공지능쪽으로 전문가가 되고 싶다면서 인공지능 공부를 제대로 한 적이 없는 것 같다. 케라스부터 해야하나 아니면 어떻게 해야하나 업무 제대로 끝맞춘게 없다. 왜냐하면 지금 잘 안되거든 deepsort는 마무리 단계이긴한데… 짜증나는건 multiprocessing 어떻게 하는거지.. 클라이밍 클라이밍 다녀와야겠다. 오늘은 너무 늦게 썻어.....","categories": ["Thought"],
         "tags": ["Thought","Essay"],
         "url": "http://localhost:4000/thought/Thought/",
         "teaser": null
